3e5e9972b0956b393181a730e0c6c3a09d954edb
sage417 <sage417@foxmail.com>
2019-05-15 21:02:20 +0800

    [Exposed-Fix]: fix random error on Android O.
     Above Android O, dex load mode has changed:
     1. dynamic loaded dex's compiler-filter is 'quicken', which is kind of interpreter mode; but before android o, dynamic loaded code is compiled by no means; (refer Android O: http://androidxref.com/8.0.0_r4/xref/art/runtime/oat_file_assistant.h#52, Android N: http://androidxref.com/7.1.2_r36/xref/art/runtime/oat_file_manager.cc#42)
     2. odex location is fixed, (dexfile/../oat/<arch>/base.odex), we can not use custom location.
     3. a new vdex is appeared, which is the same directory of odex.
    
     If the odex location is incorrect, the odex may re-generate. In VA, we force compiler-mode, this may interrupt the rule and cause random crash. So, we just assign the correct location of odex to solve the problem.

--- VirtualApp/lib/src/main/java/com/lody/virtual/os/VEnvironment.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/os/VEnvironment.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import android.os.Build;
 
 import com.lody.virtual.client.core.VirtualCore;
-import com.lody.virtual.client.env.VirtualRuntime;
 import com.lody.virtual.helper.utils.FileUtils;
 import com.lody.virtual.helper.utils.VLog;
 
 import java.io.File;
+
+import mirror.dalvik.system.VMRuntime;
+
+import static android.os.Build.VERSION_CODES.O;
 
 /**
  * @author Lody
@@ -117,7 +120,14 @@ public class VEnvironment {
     }
 
     public static File getOdexFile(String packageName) {
-        return new File(DALVIK_CACHE_DIRECTORY, "data@app@" + packageName + "-1@base.apk@classes.dex");
+        if (Build.VERSION.SDK_INT >= O) {
+            // in Android O, the oatfile is relate with classloader, we must ensure the correct location to avoid repeated load dex.
+            String instructionSet = VMRuntime.getCurrentInstructionSet.call();
+            File oatDir = ensureCreated(new File(getDataAppPackageDirectory(packageName), "oat" + File.separator + instructionSet));
+            return new File(oatDir, "base.odex");
+        } else {
+            return new File(DALVIK_CACHE_DIRECTORY, "data@app@" + packageName + "-1@base.apk@classes.dex");
+        }
     }
 
     public static File getDataAppPackageDirectory(String packageName) {
