e1f050b0410ad0324eb51b60ff76490c72a57077
sage417 <sage417@foxmail.com>
2019-06-11 00:59:51 +0800

    adapt android 8.1

--- VirtualApp/lib/src/main/java/com/lody/virtual/client/stub/DaemonService.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/client/stub/DaemonService.java
@@ -1,63 +1,95 @@
 package com.lody.virtual.client.stub;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+
+import com.lody.virtual.R;
 
 
 /**
  * @author Lody
- *
  */
 public class DaemonService extends Service {
 
     private static final int NOTIFY_ID = 1001;
+    public static final String CHANNEL_NAME = "VirtualApp background";
+    public static final String CHANNEL_ID = "VirtualApp background channel";
 
-	public static void startup(Context context) {
-		context.startService(new Intent(context, DaemonService.class));
-	}
+    public static void startup(Context context) {
+        context.startService(new Intent(context, DaemonService.class));
+    }
 
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		startup(this);
-	}
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        startup(this);
+    }
 
-	@Override
-	public IBinder onBind(Intent intent) {
-		return null;
-	}
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 
-	@Override
-	public void onCreate() {
-		super.onCreate();
+    @Override
+
+    public void onCreate() {
+        super.onCreate();
         startService(new Intent(this, InnerService.class));
-        startForeground(NOTIFY_ID, new Notification());
+        startForeground(NOTIFY_ID, createNotification(this));
 
-	}
+    }
 
-	@Override
-	public int onStartCommand(Intent intent, int flags, int startId) {
-		return START_STICKY;
-	}
+    private static Notification createNotification(final Context context) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return new Notification();
 
-	public static final class InnerService extends Service {
+        final NotificationChannel chan = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_NONE);
+        chan.setLightColor(Color.BLUE);
+        chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (notificationManager != null) {
+            notificationManager.createNotificationChannel(chan);
+        }
+
+        return new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setOngoing(true)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("VirtualApp is running in background")
+                .setPriority(NotificationManager.IMPORTANCE_MIN)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .build();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    public static final class InnerService extends Service {
 
         @Override
         public int onStartCommand(Intent intent, int flags, int startId) {
-            startForeground(NOTIFY_ID, new Notification());
+            startForeground(NOTIFY_ID, createNotification(this));
             stopForeground(true);
             stopSelf();
             return super.onStartCommand(intent, flags, startId);
         }
 
-		@Override
-		public IBinder onBind(Intent intent) {
-			return null;
-		}
-	}
+        @Override
+        public IBinder onBind(Intent intent) {
+            return null;
+        }
+    }
 
 
 }
